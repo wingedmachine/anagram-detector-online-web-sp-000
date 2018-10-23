dbc1e748cdf7e76d06412dcd3311491793ac5b45
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 20:06:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,7 @@ class Anagram
   def sort_word(word)
     word.chars.sort
   end
-  
+
   def initialize(input)
     @word = sort_word(input)
   end
@@ -14,7 +14,7 @@ class Anagram
     anagram_detector = {}
     candidates.each do |candidate|
       sorted_candidate = sort_word(candidate)
-      anagram_detector[sorted_candidate] = [] if anagram_detector.keys.include?(sorted_candidate)
+      anagram_detector[sorted_candidate] = [] if !anagram_detector.keys.include?(sorted_candidate)
       anagram_detector[sorted_candidate] << candidate
     end
     anagram_detector[word]
